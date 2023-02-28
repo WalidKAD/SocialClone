addfc76069464d8165596bb8f20a95ea374d0f3b
Walid KADDICHE <101042324+WalidKAD@users.noreply.github.com>
2023-02-28 18:30:47 +0100

    Update index.jsx

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -8,7 +8,7 @@ const LoginPage = () => {
     <Box>
       <Box
         width="100%"
-        backgroudColor={theme.palette.background.alt}
+        backgroundColor={theme.palette.background.alt}
         p="1rem 6%"
         textAlign="center"
       >
@@ -22,10 +22,10 @@ const LoginPage = () => {
         p="2rem"
         m="2rem auto"
         borderRadius="1.5rem"
-        backgroudColor={theme.palette.background.alt}
+        backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media for Sociopaths!
+          Welcome to Sociopedia, the Social Media for Sociopaths!
         </Typography>
         <Form />
       </Box>
